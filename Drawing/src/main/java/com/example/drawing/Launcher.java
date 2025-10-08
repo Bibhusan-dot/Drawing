13435ab76274e1ac19502032b8dfdda2b6635fb7
bibhusan-69 <109571270+Bibhusan-dot@users.noreply.github.com>
2025-10-08 15:00:48 +0545

    Delete Drawing directory

--- Drawing/src/main/java/com/example/drawing/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.drawing;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(Drawing.class, args);
-    }
-}
--- Drawing/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.drawing {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.drawing to javafx.fxml;
-    exports com.example.drawing;
-}
